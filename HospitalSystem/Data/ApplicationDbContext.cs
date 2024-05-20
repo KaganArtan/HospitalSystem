4a9a3ea37ce74a3fe980a0d9d57600c5e3182031
Kağan ARTAN <kagan.artan@boun.edu.tr>
2024-05-20 21:16:01 +0300

    Add project files.

---
+++ HospitalSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HospitalSystem.Models;
+
+namespace HospitalSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HospitalSystem.Models.Patient> Patient { get; set; } = default!;
+        public DbSet<HospitalSystem.Models.Appointment> Appointment { get; set; } = default!;
+    }
+}
---
+++ HospitalSystem/Models/DbClass.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Mail;
+
+namespace HospitalSystem.Models
+{
+    public class Hospital
+    {
+        public int HospitalId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public byte[] ImageData { get; set; } 
+        public List<Doctor> Doctors { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+
+
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+    }
+}
